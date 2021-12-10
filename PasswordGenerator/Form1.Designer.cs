d6c7ceb0f7c2da02adbcb57167d76cd920d937ac
mkhoyanarmen <armenmkhoyan1@gmail.com>
2021-12-10 22:45:07 +0400

    first commit

---
+++ PasswordGenerator/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace PasswordGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGeneration = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPasswordLength = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(128, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(252, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Password Generator";
+            // 
+            // btnGeneration
+            // 
+            this.btnGeneration.Location = new System.Drawing.Point(133, 163);
+            this.btnGeneration.Name = "btnGeneration";
+            this.btnGeneration.Size = new System.Drawing.Size(247, 86);
+            this.btnGeneration.TabIndex = 1;
+            this.btnGeneration.Text = "Generate";
+            this.btnGeneration.UseVisualStyleBackColor = true;
+            this.btnGeneration.Click += new System.EventHandler(this.btnGeneration_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(111, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Length (<=25)";
+            // 
+            // txtPasswordLength
+            // 
+            this.txtPasswordLength.Location = new System.Drawing.Point(203, 81);
+            this.txtPasswordLength.Name = "txtPasswordLength";
+            this.txtPasswordLength.Size = new System.Drawing.Size(154, 22);
+            this.txtPasswordLength.TabIndex = 0;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtPassword.Location = new System.Drawing.Point(84, 123);
+            this.txtPassword.Multiline = true;
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.ReadOnly = true;
+            this.txtPassword.Size = new System.Drawing.Size(351, 34);
+            this.txtPassword.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnGeneration;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 353);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtPasswordLength);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnGeneration);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Password Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGeneration;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPasswordLength;
+        private System.Windows.Forms.TextBox txtPassword;
+    }
+}
+
---
+++ PasswordGenerator/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PasswordGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGeneration_Click(object sender, EventArgs e)
+        {
+            Random PassGen = new Random();
+            StringBuilder passwd = new StringBuilder();
+            int length = int.Parse(txtPasswordLength.Text);
+            if (length > 25)
+            {
+                MessageBox.Show("Incorrect length", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            for (int i = 0; i < length; i++)
+            {
+                passwd.Append((char)PassGen.Next(50, 100));
+            }
+            txtPassword.Text = passwd.ToString();
+        }
+    }
+}
